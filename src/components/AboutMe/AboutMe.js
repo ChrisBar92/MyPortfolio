c0428f2ce6d33bfddf2d5f43d5280a6e943d60e5
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2022-12-10 03:07:30 +0100

    added section__title

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,16 +5,16 @@ import MyData from './MyData'
 
 const AboutMe = () => {
 	return (
-		<section className="section__title" id="about">
-			<div className="about container">
-				<div className="about__box">
-					<MyData />
-				</div>
-				<div className="about__photo">
-					<img src={MyImg} className="about__photo--avatar" alt="avatar Krzysztofa Bartkiewicza" />
-				</div>
+		<section className="about container" id="about">		
+			<div className="about__box">
+				<MyData />
 			</div>
-		 </section>
+			<div className="about__photo">
+				<img src={MyImg} className="about__photo--avatar" alt="avatar Krzysztofa Bartkiewicza" />
+			</div>
+			<div className="section__title">O mnie</div>
+		
+		</section>
 	)
 }
 
