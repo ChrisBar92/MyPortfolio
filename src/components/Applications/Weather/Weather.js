80d39246229afa8708aa66ff71b0e1805f0dedc2
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2022-12-15 18:59:38 +0100

    commented app weather api

--- src/components/Applications/Weather/Weather.js
+++ src/components/Applications/Weather/Weather.js
@@ -4,9 +4,9 @@ import './weather.scss'
 // import axios from 'axios';
 
 const Weather = () => {
-	const api_url = `https://api.openweathermap.org/data/2.5/weather?q=krakow`
-	const api_key = `&appid={dca2cbeccdf9e75225ebe67b5f301f72}`
-	const api_units = `&units=metric`
+	// const api_url = `https://api.openweathermap.org/data/2.5/weather?q=krakow`
+	// const api_key = `&appid={dca2cbeccdf9e75225ebe67b5f301f72}`
+	// const api_units = `&units=metric`
 
 	return <div></div>
 }
